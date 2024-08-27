9abce73bd3f6a636bd3be6a76e764529ef08ecfc
JaripitiChandu <Jaripiti.Chandu@Xoriant.com>
2024-08-27 10:41:24 +0530

    Create MissingNumber.py

---
+++ MissingNumber.py
@@ -0,0 +1,8 @@
+def missingNumber(self, n, arr):
+        
+        sum = int(n*(n+1)/2)
+        cal_sum=0
+        for i in arr:
+            cal_sum+=i
+        return sum-cal_sum
+        # code here
