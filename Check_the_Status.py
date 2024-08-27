289f6d359498ce4a7d778c684d158f19d1fb982e
JaripitiChandu <Jaripiti.Chandu@Xoriant.com>
2024-08-27 10:35:14 +0530

    Update Check_the_Status.py

--- Check_the_Status.py
+++ Check_the_Status.py
@@ -2,11 +2,7 @@ def check_status(a, b, flag):
     ##Your code here
     ##Either True or False is returned
     
-    if a>0 and b<0 and flag==False:
-        return True
-    elif a<0 and b>0 and flag==False:
-        return True
-    elif a<0 and b<0 and flag==True:
+    if (((a>0 and b<0) or (a<0 and b>0)) and flag==False) or (a<0 and b<0 and flag==True):
         return True
     else:
         return False
